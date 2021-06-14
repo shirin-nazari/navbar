256d51028575a731a5e7cb3205b0d2eba7ad6415
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-06-14 12:19:05 +0430

    responsive

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const header = document.querySelector("header");
-const ulMenu = document.querySelector("ul");
-header.addEventListener("fullscreenchange", () => {
-  console.log(screen);
-  if (screen.availWidth < 1024) {
-    ulMenu.style.display = "none";
-    const icons = document.createElement("i");
-    icons.className = "fas fa-bars";
-    header.append(icons);
-  }
+const ulMenu = document.querySelector(".ul-nav");
+const li = document.querySelectorAll(".ul-nav li");
+const icons = document.querySelector(".line");
+// header.addEventListener("fullscreenchange", () => {
+//   console.log(screen);
+//   if (screen.availWidth < 1024) {
+//     ulMenu.style.display = "none";
+//     const icons = document.createElement("i");
+//     icons.className = "fas fa-bars";
+//     header.append(icons);
+//   }
+// });
+icons.addEventListener("click", () => {
+  ulMenu.classList.toggle("nav-active");
+  // li.style.opacity = "1%";
 });
